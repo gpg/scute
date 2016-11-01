26808262a25b64483346d51db56ea82a342a6fbe
NIIBE Yutaka <gniibe@fsij.org>
2016-11-02 07:52:50 +0900

    autogen.sh: Update from gnupg.
    --
    Detection of .git should be -e so that git worktree can be used.
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- autogen.sh
+++ autogen.sh
@@ -214,7 +214,7 @@ if [ "$myhost" = "find-version" ]; then
     esac
 
     beta=no
-    if [ -d .git ]; then
+    if [ -e .git ]; then
       ingit=yes
       tmp=$(git describe --match "${matchstr1}" --long 2>/dev/null)
       if [ -n "$tmp" ]; then
@@ -423,7 +423,7 @@ if [ -d .git ]; then
     [ -z "${SILENT}" ] && cat <<EOF
 *** Activating trailing whitespace git pre-commit hook. ***
     For more information see this thread:
-      http://mail.gnome.org/archives/desktop-devel-list/2009-May/msg00084html
+      http://mail.gnome.org/archives/desktop-devel-list/2009-May/msg00084.html
     To deactivate this pre-commit hook again move .git/hooks/pre-commit
     and .git/hooks/pre-commit.sample out of the way.
 EOF
