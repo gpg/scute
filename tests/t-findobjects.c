9961bd1e094f1af32b4aa7838ddbc07a01da2a89
Werner Koch <wk@gnupg.org>
2021-01-07 15:18:10 +0100

    Minor fixes for a release
    * doc/Makefile.am (DISTCLEANFILES): Add yat2m files.
    * doc/scute.texi: Minor fixes.
    * tests/t-findobjects.c (main): Add one test case.

--- tests/t-findobjects.c
+++ tests/t-findobjects.c
@@ -64,6 +64,18 @@ main (int argc, char *argv[])
       CK_ATTRIBUTE attr[]
 	= { { CKA_TOKEN, &cert_token, sizeof (cert_token) },
 	    { CKA_CLASS, &cert_class, sizeof (cert_class) } };
+      unsigned char issuer[] =
+        "\x30\x78\x31\x0B\x30\x09\x06\x03\x55\x04\x06\x13\x02\x44\x45\x31"
+        "\x16\x30\x14\x06\x03\x55\x04\x0A\x13\x0D\x67\x31\x30\x20\x43\x6F"
+        "\x64\x65\x20\x47\x6D\x62\x48\x31\x10\x30\x0E\x06\x03\x55\x04\x0B"
+        "\x13\x07\x54\x65\x73\x74\x6C\x61\x62\x31\x1E\x30\x1C\x06\x03\x55"
+        "\x04\x03\x13\x15\x67\x31\x30\x20\x43\x6F\x64\x65\x20\x54\x45\x53"
+        "\x54\x20\x43\x41\x20\x32\x30\x31\x39\x31\x1F\x30\x1D\x06\x09\x2A"
+        "\x86\x48\x86\xF7\x0D\x01\x09\x01\x16\x10\x69\x6E\x66\x6F\x40\x67"
+        "\x31\x30\x63\x6F\x64\x65\x2E\x63\x6F\x6D";
+      CK_ATTRIBUTE attr2[]
+	= { { CKA_ISSUER, issuer, sizeof issuer - 1},
+	    { CKA_SERIAL_NUMBER, "\x02\x01\x01", 3 } };
 
       printf ("%2i. Slot ID %lu\n", i, slots[i]);
       err = C_OpenSession (slots[i], CKF_SERIAL_SESSION, NULL, NULL,
@@ -87,6 +99,23 @@ main (int argc, char *argv[])
 
       printf ("    Template Search: Token, Class\n");
       err = C_FindObjectsInit (session, attr, DIM (attr));
+      fail_if_err (err);
+
+      do
+	{
+	  err = C_FindObjects (session, &object, 1, &count);
+	  fail_if_err (err);
+
+	  if (count)
+	    printf ("    Object Handle: %lu\n", object);
+	}
+      while (count);
+
+      err = C_FindObjectsFinal (session);
+      fail_if_err (err);
+
+      printf ("    Template Search: Issuer, Serial\n");
+      err = C_FindObjectsInit (session, attr2, DIM (attr2));
       fail_if_err (err);
 
       do
